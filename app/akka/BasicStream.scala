231a4e04a9bddfedfdbb85aff1fae2d4a77460d7
abdelillah-tech <58631728+abdelillah-tech@users.noreply.github.com>
2020-04-25 09:17:38 +0200

    Update BasicStream.scala

--- app/akka/BasicStream.scala
+++ app/akka/BasicStream.scala
@@ -1,5 +1,15 @@
 package com.particeep.test.akka
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.Sink
+import akka.NotUsed
+import scala.concurrent.Future
+import akka.Done
+
+
 /**
   * Question about Akka Stream framework https://doc.akka.io/docs/akka/current/stream/index.html
   *
@@ -16,15 +26,15 @@ object BasicStream {
     val numberSource: Source[Int, NotUsed] = Source.fromIterator(() => numbers.iterator)
 
     //Only let pass even number through the flow
-    val isEvenFlow: Flow[Int, Int, NotUsed] = ???
+    val isEvenFlow: Flow[Int, Int, NotUsed] = Flow[Int].filter(_ % 2 == 0)
 
     //Create a Source of even numbers by combining the number Source with the even Flow
-    val evenNumberSource: Source[Int, NotUsed] = ???
+    val evenNumberSource: Source[Int, NotUsed] = numberSource.via(isEvenFlow)
 
     //A Sink that will write its input onto the console
-    val consoleSink: Sink[Int, Future[Done]] = ???
+    val consoleSink: Sink[Int, Future[Done]] = Sink.foreach[Int](println)
 
     //Connect the Source with the Sink and run it
-    ???
+    evenNumbersSource.runWith(consoleSink)
   }
 }
